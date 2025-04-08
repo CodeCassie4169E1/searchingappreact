a5e63cea91a9ea0b82ad12ae0e0d148798e2c941
CodeCassie4169E1 <clipcassie@yahoo.com>
2025-04-08 12:25:20 -0500

    search engine commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import "./App.css";
+import "./Dictionary";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary </header>
-      <a
-        className="App-link"
-        href="https://github.com/CodeCassie4169E1/searchingappreact"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Code
-      </a>
-      by Cassie Sichmeller
+      <div className="container">
+        <header className="App-header">Dictionary </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          <a
+            className="App-link"
+            href="https://github.com/CodeCassie4169E1/searchingappreact"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Code
+          </a>
+          by Cassie Sichmeller
+        </footer>
+      </div>
     </div>
   );
 }
